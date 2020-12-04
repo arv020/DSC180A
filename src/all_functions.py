b248bf789005725d524478faa76383c5c1ecf156
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-12-03 23:31:40 -0800

    Update all_functions.py

--- src/all_functions.py
+++ src/all_functions.py
@@ -45,35 +45,35 @@ def modify_data(raw_data):
     modified_data = pd.DataFrame({'packet_sizes': pd.to_numeric(new_pksize), 'packet_dir': pd.to_numeric(new_pkdir)})
     return modified_data
     
-def prop_pksize_dir12(tbl, num):
-    proportion = tbl[tbl["packet_sizes"] < num]["packet_dir"].value_counts()[2]/ tbl[tbl["packet_sizes"] < num]["packet_dir"].value_counts()[1]
+def prop_pksize_dir12(tbl):
+    proportion = tbl[tbl["packet_sizes"] < 200]["packet_dir"].value_counts()[2]/ tbl[tbl["packet_sizes"] < 200]["packet_dir"].value_counts()[1]
     
     return proportion
     
     
     
-def binarymin_packetsizes(modified_data, num):
-    output = (modified_data["packet_sizes"].min() <= num)
+def binarymin_packetsizes(modified_data):
+    output = (modified_data["packet_sizes"].min() <= 32)
     if output == 1:
         return "Streaming"
     else:
         return "Not Streaming"
         
         
- def binary_max_pksz(tbl, num):
-    num_packets = tbl[tbl["packet_sizes"] >= num].size
+ def binary_max_pksz(tbl):
+    num_packets = tbl[tbl["packet_sizes"] >= 1400].size
     if num_packets > 0:
         return "Streaming"
     else:
         return "Not Streaming"
         
-def prop_range200_400_dir1(tbl, num1, num2):
-    a = len(tbl[(tbl["packet_sizes"] > num1) & (tbl["packet_sizes"] < num2) & (tbl["packet_dir"] == 1)])
+def prop_range200_400_dir1(tbl):
+    a = len(tbl[(tbl["packet_sizes"] > 200) & (tbl["packet_sizes"] < 400) & (tbl["packet_dir"] == 1)])
     b = len(tbl[tbl["packet_dir"] == 1])
     return a/b
     
-def prop_200toentire(tbl, num):
-    proportion = tbl[tbl["packet_sizes"] < num]["packet_dir"].size/tbl["packet_sizes"].size
+def prop_200toentire(tbl):
+    proportion = tbl[tbl["packet_sizes"] < 200]["packet_dir"].size/tbl["packet_sizes"].size
     return proportion
     
     
