55e3d0f26f432fbd65e60338c35160ebb238e107
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2020-12-03 22:15:24 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import numpy as np
 import pylab as pl
-from statistics import mode
-from sklearn.linear_model import LogisticRegression
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
 
 sys.path.insert(0, 'src')
-from data import modify_data
-from analysis import pktdir_vs_pktsze_int pktdir_vs_pktsze_vid
-from features import binarymean_packetsizes binary_max_pksz prop_pksize_dir12 prop_pksize_dir12 classifier
-from models import filter_out filter_out_table classify_training table_output_col build_df build_model final_output
+from all_features import filter_out_table table_output_col build_df build_model
+
 from utils import convert_notebook
 
 
@@ -31,30 +26,20 @@ def main(targets):
         filepath = '/teams/DSC180A_FA20_A00/b05vpnxray/data/unzipped'
         data = os.listdir(filepath)
         
-        model_df = filter_out_table()
+        model_df = build_df(table_output_col(filter_out_table()))
         
         #this creates the data df that builds the model
-        model_df = table_output_col(model_df)
-        
 
-        
-        #data = generate_data(**data_config)
-        #save_data(data, **data_config)
-        
-        
-            
-     
             
      if 'models' in targets:
         
-        build_df(model_df)
+        #here we have a Random Forest Classifier Model built
+        output = build_df(model_df)
         
-        #at this point we have a logistic regression model (reg)
-        build_model(model_df)
-     
+        
     
      #at the end, we want to return Streaming/Not streaming an input
-     return "success"
+     return output
         
         
         
